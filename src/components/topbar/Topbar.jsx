7e7c9362dc2f809b38ad7290e6a380324108f2b1
shahryar <shahryar.asgit@gmail.com>
2024-09-20 12:17:44 +0330

    Top Bar complete

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+import './Topbar.css'
+export default function Topbar() {
+  return (
+    <div className='topbar'>
+      <div className='topbarWrapper'>
+
+        <div className="topLeft">
+            <span className='logo'>SabzLearn </span>
+        </div>
+
+        <div className="topRight">
+            <div className="topbarIconContainer">
+              <NotificationsIcon/>
+              <span className='topIconBadge'>2</span>
+            </div>
+            <div className="topbarIconContainer">
+              <LanguageIcon/>
+              <span className='topIconBadge'>2</span>
+            </div>
+            <div className="topbarIconContainer">
+              <SettingsIcon/>
+              <span className='topIconBadge'>2</span>
+            </div>
+            <img className='topAvatar' src="images/profile.jfif" alt="" />
+
+            
+        </div>
+      </div>
+    </div>
+  )
+}
