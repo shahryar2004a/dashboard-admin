0d4f482ff17e9aedef3d034e0eaabf11c9149371
shahryar <shahryar.asgit@gmail.com>
2024-09-20 14:45:46 +0330

    add chart

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Feature from '../../components/features/Feature'
+import Chart from '../../components/Chart/Chart'
+import xAxisData from '../../datas'
 
-
+import './Home.css'
 export default function Home() {
   return (
-    <>
+    <div className='home'>
       <Feature/>
-    </>
+      <Chart grid title="Month Sale" data={xAxisData} dataKey="Sale"/>
+    </div>
   )
 }
