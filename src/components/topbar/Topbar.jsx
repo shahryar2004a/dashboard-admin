9e32dcc3d4f3e4e71d735c18091847b8083bb8cb
shahryar <146966167+shahryar2004a@users.noreply.github.com>
2025-06-09 19:30:41 +0330

    Update Topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -10,7 +10,7 @@ export default function Topbar() {
       <div className='topbarWrapper'>
 
         <div className="topLeft">
-            <span className='logo'>SabzLearn </span>
+            <span className='logo'>Shahryar</span>
         </div>
 
         <div className="topRight">
